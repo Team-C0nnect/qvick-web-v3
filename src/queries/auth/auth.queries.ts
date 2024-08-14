bc8fccf5a30666006aee37fecf3fbd5b9be647e6
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-14 12:11:15 +0900

    feat :: Auth query #8

---
+++ src/queries/auth/auth.queries.ts
@@ -0,0 +1,12 @@
+import AuthRepositoryImpl from "src/repositories/authRepository/authRepositoryImpl";
+import { useMutation } from "react-query";
+
+export const useLoginMutation = () => {
+    const mutation = useMutation(AuthRepositoryImpl.login);
+    return mutation;
+}
+
+export const useSignUpMutation = () => {
+    const mutation = useMutation(AuthRepositoryImpl.signUp);
+    return mutation;
+}
