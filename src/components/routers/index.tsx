0104c6ce3a69ad6a8a481022102189bfb63d4f3c
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-24 11:30:02 +0900

    feat :: notFound페이지 #11

--- src/components/routers/index.tsx
+++ src/components/routers/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Sign from "src/pages/auth/page"
+import Notfound from "src/components/common/notfound";
 
 const Routers = () => {
     return (
         <Routes>
             <Route path="/sign" element={<Sign />} />
+            <Route path="*" element={<Notfound />} />
         </Routes>
     )
 }
