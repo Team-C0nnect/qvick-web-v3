1e850d0f027ba135bbb6abedf09a15c3e685a182
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-27 19:56:06 +0900

    feat :: header style #11

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import { useGetProfile } from "src/queries/profile/profile.queries";
 import { tokenCheck } from "src/libs/tokenCheck/tokenCheck";
 import logoImg from "src/assets/img/logo.svg";
+import profileImg from "src/assets/img/profileImg.svg";
+import * as S from "src/components/common/header/style";
 
 const Header = () => {
     const { data } = useGetProfile();
     const { getTokenCheck } = tokenCheck();
 
     return (
-        <div className="headWrap">
+        <S.headWrap>
             {getTokenCheck() ? (
                 <>
-                    <img src={logoImg} alt="logo" />
-                    <span className="name">{data?.name}</span>
+                    <S.logoImg src={logoImg} alt="logo" />
+                    <S.nameContainer>
+                        <S.profileImg src={profileImg} alt="profileImg" />
+                        <S.profileName className="name">{data?.name}</S.profileName>
+                    </S.nameContainer>
                 </>
             ) : null}
-        </div>
+        </S.headWrap>
     );
 };
 
