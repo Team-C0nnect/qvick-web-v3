7e12e9614cf42b2b2ee66d597f706f0c96b846f8
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-27 10:03:20 +0900

    feat :: profile queries #11

---
+++ src/queries/profile/profile.queries.ts
@@ -0,0 +1,18 @@
+import { useMutation, useQuery } from "react-query";
+import { AxiosError } from "axios";
+import { profileType } from "src/types/profile/profile.type";
+import profileRepositoryImpl from "src/repositories/profileRepository/profileRepositoryImpl";
+import { qvickQueryKey } from "src/queries/queriesKey";
+
+export const usePatchProfileMutation = () => {
+    return useMutation(profileRepositoryImpl.patchProfile);
+};
+
+export const useGetProfile = () => {
+    return useQuery<profileType, AxiosError, profileType, string[]>({
+        queryKey: [qvickQueryKey.profile.getProfile],
+        queryFn: profileRepositoryImpl.getProfileInfo,
+        staleTime: 30 * 1000,
+        cacheTime: 60 * 1000,
+    });
+};
