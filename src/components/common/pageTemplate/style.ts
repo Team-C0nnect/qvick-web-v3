bb66b893e738fe52db45fb9aed31a7a603fe6dbd
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-28 09:34:03 +0900

    feat :: pageTemplate #11

---
+++ src/components/common/pageTemplate/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const layoutWrap = styled.div`
+    height: 100vh;
+    display: flex;
+    user-select: none;
+    background-color: #f7fff3;
+    overflow: hidden;
+`;
