194f5fa3ab1e02f3076f0d30f0742c3a6e6fead3
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-29 13:52:58 +0900

    fix :: style 수정 #10

--- src/components/common/pageTemplate/style.ts
+++ src/components/common/pageTemplate/style.ts
@@ -4,6 +4,6 @@ export const layoutWrap = styled.div`
     height: 100vh;
     display: flex;
     user-select: none;
-    background-color: #f7fff3;
+    background-color: #FFFFFF;
     overflow: hidden;
 `;
