5c0382199314d355452750f20211ee32f8c90569
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-01 03:16:22 +0900

    fix :: 파일명 수정 #10
    더 기능과 맞는 파일로 이름변경

--- src/components/auth/Signup/index.tsx
+++ src/components/auth/Signup/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import SingUpInformation from "src/components/auth/Signup/singupInformation";
 
 const Signup = () => {
     return (
-        <></>
+        <div>
+            <SingUpInformation/>
+        </div>
     )
 }
 
