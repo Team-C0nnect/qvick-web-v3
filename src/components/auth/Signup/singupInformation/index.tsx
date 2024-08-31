2ef49e6874d4f565861e315e18da2f102b6d1af3
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-01 03:15:47 +0900

    feat :: 회원가입 정보 뷰 #10

--- src/components/auth/Signup/singupInformation/index.tsx
+++ src/components/auth/Signup/singupInformation/index.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import SideBarImg from "src/assets/img/normalSide.svg";
+import userImg from "src/assets/img/userImg.svg";
+import lockImg from "src/assets/img/Locker.svg";
+import * as S from "src/components/auth/Signup/singupInformation/style";
 
-const singupInformation = () => {
+const SignUpInformation = () => {
     return (
-        <></>
-    )
-}
+        <S.Container>
+            <S.SideImage src={SideBarImg} alt="사이드 이미지" />
+            <S.SignUpWrap>
+                <S.InstructionText>회원가입을 위한 정보를 입력해주세요.</S.InstructionText>
+                <S.InformationWrap>
+                    <S.InputGroup>
+                        <S.Label>이름</S.Label>
+                        <S.InputIcon src={userImg} alt="유저 아이콘" />
+                        <S.Input placeholder="이름을 입력해주세요" />
+                    </S.InputGroup>
+                    <S.InputGroup>
+                        <S.Label>이메일</S.Label>
+                        <S.InputIcon src={userImg} alt="유저 아이콘" />
+                        <S.Input placeholder="이메일을 입력해주세요" />
+                    </S.InputGroup>
+                    <S.InputGroup>
+                        <S.Label>비밀번호</S.Label>
+                        <S.InputIcon src={lockImg} alt="비밀번호 아이콘" />
+                        <S.Input type="password" placeholder="비밀번호를 입력해주세요" />
+                    </S.InputGroup>
+                    <S.Button>회원가입</S.Button>
+                </S.InformationWrap>
+            </S.SignUpWrap>
+        </S.Container>
+    );
+};
 
-export default singupInformation;
+export default SignUpInformation;
--- src/components/auth/Signup/singupInformation/style.ts
+++ src/components/auth/Signup/singupInformation/style.ts
@@ -1,1 +1,90 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    width: 100vw; 
+    height: 100vh; 
+`;
+
+export const SideImage = styled.img`
+    flex: 2;
+    height: 100%;
+    object-fit: cover;
+`;
+
+export const SignUpWrap = styled.div`
+    flex: 3;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+`;
+
+export const InstructionText = styled.span`
+    margin-bottom: 40px;
+    font-weight: 550;
+    font-size: 35px;
+    text-align: center;
+`;
+
+export const InformationWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+    max-width: 500px; 
+    align-items: center;
+    justify-content: center; 
+`;
+
+export const InputGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    position: relative;
+    width: 100%;
+`;
+
+export const Label = styled.label`
+    font-size: 17px;
+    margin-bottom: 5px;
+    margin-left: 5px;
+    color: #A1A1A1;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    padding: 10px 10px 10px 40px;
+    height: 40px;
+    border-radius: 8px;
+    border: 1px solid #ccc;
+    font-size: 16px;
+`;
+
+export const InputIcon = styled.img`
+    position: absolute;
+    left: 10px;
+    top: 62%; 
+    transform: translateY(-50%);
+    width: 15px;
+    height: 15px;
+`;
+
+export const Button = styled.button`
+    width: 550px; 
+    height: 55px;
+    border-radius: 8px;
+    border: none;
+    background-color: #0389FF;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    margin-top: 10px;
+    margin-left: 50px;
+    &:hover {
+        background-color: #0278d4;
+    }
+`;
