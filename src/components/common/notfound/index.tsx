894231b7ba6026538f87e37dd9a978ddbd410f2f
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-01 19:55:18 +0900

    fix :: notFound Page 경로 수정 #11

--- src/components/common/notfound/index.tsx
+++ src/components/common/notfound/index.tsx
@@ -6,14 +6,14 @@ const Notfound = () => {
     const navigate = useNavigate();
 
     const handleBack = () => {
-        navigate("/main");
+        navigate("/sign");
     }
 
     return (
         <S.notFoundWrap>
             <S.Title>404 Error</S.Title>
             <S.Description>존재하지 않는 페이지 입니다.</S.Description>
-            <S.returnButton onClick={handleBack}>메인화면으로 돌아가기</S.returnButton>
+            <S.returnButton onClick={handleBack}>로그인화면으로 돌아가기</S.returnButton>
         </S.notFoundWrap>
     );
 };
