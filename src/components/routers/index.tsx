9e4cb9e2d0ac85ee9268f11a25d8db2227782f79
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-09 14:14:04 +0900

    feat :: 기본셋팅 #20

--- src/components/routers/index.tsx
+++ src/components/routers/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Sign from "src/pages/auth/page";
 import Notfound from "src/components/common/notfound";
+import MainPage from "src/pages/Main/page";
 
 const Routers = () => {
     return (
         <Routes>
             <Route path="/sign" element={<Sign />} />
             <Route path="*" element={<Notfound />} />
+            <Route path="/main" element={<MainPage />} />
         </Routes>
     )
 }
---
+++ src/pages/Main/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MainComponents from "src/components/home/Main/index";
+
+const Page = () => {
+    return (
+        <MainComponents />
+    )
+}
+
+export default Page;
