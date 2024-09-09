7f7c46e89eae41572f2a0bf664be9fbd5686b108
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-10 08:32:05 +0900

    fix :: header UI 수정 #20

--- src/components/common/header/style.ts
+++ src/components/common/header/style.ts
@@ -3,33 +3,53 @@ import styled from "styled-components";
 export const headWrap = styled.div`
     width: 100%;
     display: flex;
-    position: absolute;
     align-items: center;
-    justify-content: flex-end;
+    justify-content: space-between;
+    padding: 0 20px;
     background-color: #0389FF;
+    position: relative;
+    height: 80px; 
 `;
 
 export const logoImg = styled.img`
-    width: 32px;
-    height: 32px;
+    width: 90px; 
+    height: 90px;
     flex-shrink: 0;
 `;
 
+export const navBar = styled.nav`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
+`;
+
 export const nameContainer = styled.div`
-    width: 100px;
-    height: 100px;
+    display: flex;
+    align-items: center;
     background-color: #FFFFFF;
-    border-radius: 50%;
+    border-radius: 50px;
+    padding: 10px 20px;
+    gap: 10px;
+    margin-right: 25px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    width: 150px;
+    height: 25px;
+;
 `;
 
 export const profileImg = styled.img`
-    width: 20px;
-    height: 20px;
+    width: 30px; 
+    height: 30px;
+    border-radius: 50%;
 `;
 
 export const profileName = styled.span`
-    font-size: 16px;
-    font-style: normal;
+    font-size: 18px;
     font-weight: 700;
-    padding-left: 11px;
+    color: #000000;
+    margin-left: 20px;
 `;
