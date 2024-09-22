53172e0bb5b4116c8b4c4fd006306d02a11b2f14
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-22 21:02:26 +0900

    feat :: checkMember #25

--- src/components/home/CkMember/index.tsx
+++ src/components/home/CkMember/index.tsx
@@ -1,10 +1,58 @@
 import React from "react";
 import * as S from "src/components/home/CkMember/style";
+import { useGetCheckedMembersTrue } from "src/queries/member/member.queries";
 
 const CkMember = () => {
+    const {data, isLoading, isError} =  useGetCheckedMembersTrue()
+
+    if(isLoading) {
+        return <S.memberContainer>로딩중...</S.memberContainer>
+    }
+
+    if(isError) {
+        return <S.memberContainer>에러가 발생했습니다.</S.memberContainer>
+    }
+
+    const members = data?.data;
+
+    if (!members || !Array.isArray(members)) {
+        return <S.memberContainer>데이터가 없습니다.</S.memberContainer>;
+    }
+
+    const checkedMembers = members.filter(member => member.checked);
+
+    if (checkedMembers.length === 0) {
+        return <S.memberContainer>출석한 학생이 없습니다.</S.memberContainer>;
+    }
+
     return (
         <S.memberContainer>
-            출석자 명단 리스트 입니다.
+            <S.tableWrapper>
+                <S.table>
+                    <thead>
+                    <tr>
+                        <S.th>학번</S.th>
+                        <S.th>성별</S.th>
+                        <S.th>이름</S.th>
+                        <S.th>출석 여부</S.th>
+                        <S.th>전화번호</S.th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {checkedMembers.map((member) => (
+                        <S.tr key={member.stdId}>
+                            <S.td>{member.stdId}</S.td>
+                            <S.td>{member.gender === 'MALE' ? '남' : '여'}</S.td>
+                            <S.td>{member.name}</S.td>
+                            <S.td style={{ color: 'green' }}>
+                                출석
+                            </S.td>
+                            <S.td>{member.phoneNum}</S.td>
+                        </S.tr>
+                    ))}
+                    </tbody>
+                </S.table>
+            </S.tableWrapper>
         </S.memberContainer>
     )
 }
