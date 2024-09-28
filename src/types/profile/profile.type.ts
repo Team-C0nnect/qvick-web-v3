629f055bfa2c8be4bb5c52f14850c5ae8bd5dc1f
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-09-28 12:56:54 +0900

    fix :: data타입 오류 해결 #25

--- src/types/profile/profile.type.ts
+++ src/types/profile/profile.type.ts
@@ -1,14 +1,14 @@
 export interface profileType {
-    data: {
-        email : string,
-        name : string,
-        password : string,
-        stdId : string,
-        room : string,
-        phoneNum : string,
-        gender : string,
-        userRole : string,
-        isChecked : false,
-        checkedDate : string
-    }
+   data: {
+       email : string,
+       name : string,
+       password : string,
+       stdId : string,
+       room : string,
+       phoneNum : string,
+       gender : string,
+       userRole : string,
+       isChecked : false,
+       checkedDate : string
+   }
 }
