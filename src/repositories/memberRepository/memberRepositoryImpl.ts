54f2faac9ae5993425f03760c95cf4e14266dc97
박규민 <131234408+kyumin7487@users.noreply.github.com>
2025-04-11 21:30:22 +0900

    fix :: size 수정 #20

--- src/repositories/memberRepository/memberRepositoryImpl.ts
+++ src/repositories/memberRepository/memberRepositoryImpl.ts
@@ -3,12 +3,12 @@ import { memberType } from "src/types/member/member.type";
 import { memberRepository } from "./memberRepository";
 
 class MemberRepositoryImpl implements memberRepository {
-    public async getAllMembers(page: number = 1, size: number = 10): Promise<memberType[]> {
+    public async getAllMembers(page: number = 1, size: number = 1000): Promise<memberType[]> {
         const { data } = await QvickAxios.get("/user/list", { params: { page, size } });
         return data.data.filter((member: memberType) => member.userRole === "USER");
     }
 
-    public async getCheckedMembers(checked: boolean, page: number = 1, size: number = 10): Promise<memberType[]> {
+    public async getCheckedMembers(checked: boolean, page: number = 1, size: number = 1000): Promise<memberType[]> {
         const { data } = await QvickAxios.get("/check", { params: { checked, page, size } });
         return data.data.filter((member: memberType) => member.userRole === "USER");
     }
